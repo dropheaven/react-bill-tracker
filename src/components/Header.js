50edd46b459dbaf761e183ac3df8e51eb696f626
Saad Ahmed <dropheaven@live.com>
2019-06-11 00:33:18 -0400

    changed header element to a div

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 const Header = () => (
-  <header>
+  <div id="sidebar">
     <h1>BillTracker</h1>
     <NavLink to="/" exact={true}>
       Dashboard
@@ -13,7 +13,7 @@ const Header = () => (
     <NavLink to="/edit" exact={true}>
       Edit
     </NavLink>
-  </header>
+  </div>
 );
 
 export default Header;
