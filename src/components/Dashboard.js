6d2e1bfc5e60097eb4b814b01b77374365c123d1
Saad Ahmed <dropheaven@live.com>
2019-06-11 23:03:00 -0400

    testing Expense components with props

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,8 +3,18 @@ import Expense from "./Expense";
 
 const Dashboard = () => (
   <div id="expenses">
-    <Expense name="Electricity" amount="$65" category="utilities" />
-    <Expense name="Internet" amount="$80" category="utilities" />
+    <Expense
+      name="Electricity"
+      amount="$65"
+      dueDate="July 5, 2019"
+      category="utilities"
+    />
+    <Expense
+      name="Internet"
+      amount="$80"
+      dueDate="Dec 12, 2020"
+      category="utilities"
+    />
   </div>
 );
 
