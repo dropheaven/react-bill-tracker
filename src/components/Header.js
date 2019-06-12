b52edb861ede2c33d54a798aa3dd9970b34933dc
Saad Ahmed <dropheaven@live.com>
2019-06-11 23:04:46 -0400

    removed edit link for header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,14 +4,11 @@ import { NavLink } from "react-router-dom";
 const Header = () => (
   <div id="sidebar">
     <h1>BillTracker</h1>
-    <NavLink to="/" exact={true}>
+    <NavLink exact to="/">
       Dashboard
     </NavLink>
     <NavLink to="/create" exact={true}>
       Create Expense
-    </NavLink>
-    <NavLink to="/edit" exact={true}>
-      Edit
     </NavLink>
   </div>
 );
