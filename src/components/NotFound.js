d61f0d7ee10786dafc5469e060dc64f74d591cbb
Saad Ahmed <dropheaven@live.com>
2019-06-12 21:46:04 -0400

    link to dashboard from 404 page

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,8 +1,10 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const NotFound = () => (
   <div>
     <p>404</p>
+    <Link to="/">Return to Dashboard</Link>
   </div>
 );
 
