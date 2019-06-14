3f90bf5c70405b28216be12d939ea7ffef01f7af
Saad Ahmed <dropheaven@live.com>
2019-06-14 00:21:54 -0400

    moved due date and date into separate header tags

--- src/components/Expense.js
+++ src/components/Expense.js
@@ -5,7 +5,8 @@ const Expense = props => (
     <div className="card-body">
       <h5 className="card-title">{props.name}</h5>
       <h6 className="card-subtitle mb-2 text-muted">{props.category}</h6>
-      <h4 className="card-text">Due on {props.dueDate}</h4>
+      <h5 className="card-text">Due on</h5>
+      <h4 className="card-text">{props.dueDate}</h4>
       <h1 className="card-text">{props.amount}</h1>
       <a href="/" className="card-link">
         details
