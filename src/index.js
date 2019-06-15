32b6dea8f5e013fe027cccaf07737afb8d9b4aa0
Saad Ahmed <dropheaven@live.com>
2019-06-14 23:13:53 -0400

    importing store, action generators, and expense reducer.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import AppRouter from "./routers/AppRouter";
+import configureStore from "./store/configureStore";
+import { addExpense } from "./actions/expenses";
 import "bootstrap/dist/css/bootstrap.css";
 import "./styles/main.css";
+
+const store = configureStore();
+store.dispatch(addExpense({ id: 1, name: "water bill", amount: 7000 }));
+store.dispatch(addExpense({ id: 1, name: "gas bill", amount: 3000 }));
+console.log(store.getState());
 
 ReactDOM.render(<AppRouter />, document.getElementById("root"));
