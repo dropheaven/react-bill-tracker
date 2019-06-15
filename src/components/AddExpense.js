d7e767a6c17b9c36f12b5aace58a2b703a56ea60
Saad Ahmed <dropheaven@live.com>
2019-06-15 18:24:33 -0400

    adding new expense to store and redirecting to dashboard

--- src/components/AddExpense.js
+++ src/components/AddExpense.js
@@ -1,11 +1,18 @@
 import React from "react";
+import { connect } from "react-redux";
 import ExpenseForm from "./ExpenseForm";
+import { addExpense } from "../actions/expenses";
 
-const AddExpense = () => (
+const AddExpense = props => (
   <div className="container text-center" id="expense-form">
     <h1 className="text-center mb-3">New Expense</h1>
-    <ExpenseForm />
+    <ExpenseForm
+      onSubmit={expense => {
+        props.dispatch(addExpense(expense));
+        props.history.push("/");
+      }}
+    />
   </div>
 );
 
-export default AddExpense;
+export default connect()(AddExpense);
