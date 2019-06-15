cac08479ed6b75d7c9cf263bff25c7cfa057b183
Saad Ahmed <dropheaven@live.com>
2019-06-15 00:38:20 -0400

    destructuring props object, using Link component, and changed a few properties.

--- src/components/Expense.js
+++ src/components/Expense.js
@@ -1,19 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const Expense = props => (
+const Expense = ({ name, category, dueDate, amount }) => (
   <div className="card">
     <div className="card-body">
-      <h5 className="card-title">{props.name}</h5>
-      <h6 className="card-subtitle mb-2 text-muted">{props.category}</h6>
+      <h5 className="card-title">{name}</h5>
+      <h6 className="card-subtitle mb-2 text-muted">{category}</h6>
       <h5 className="card-text">Due on</h5>
-      <h4 className="card-text">{props.dueDate}</h4>
-      <h1 className="card-text">{props.amount}</h1>
-      <a href="/" className="card-link">
-        details
-      </a>
-      <a href="/" className="card-link">
-        edit
-      </a>
+      <h4 className="card-text">{dueDate}</h4>
+      <h1 className="card-text">${amount * 0.01}</h1>
+      <Link to="/">
+        <button type="button" className="btn btn-primary">
+          details
+        </button>
+      </Link>
+      <Link to="/">
+        <button type="button" className="btn btn-primary">
+          edit
+        </button>
+      </Link>
     </div>
   </div>
 );
