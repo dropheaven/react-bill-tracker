c1e36c204093b81a3da6e49190055287a5fb651c
Saad Ahmed <dropheaven@live.com>
2019-06-15 12:54:07 -0400

    initialized a Provider component and added a few expenses to the store using dispatch

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import AppRouter from "./routers/AppRouter";
 import configureStore from "./store/configureStore";
 import { addExpense } from "./actions/expenses";
@@ -7,8 +8,48 @@ import "bootstrap/dist/css/bootstrap.css";
 import "./styles/main.css";
 
 const store = configureStore();
-store.dispatch(addExpense({ id: 1, name: "water bill", amount: 7000 }));
-store.dispatch(addExpense({ id: 1, name: "gas bill", amount: 3000 }));
-console.log(store.getState());
+store.dispatch(
+  addExpense({
+    id: 1,
+    category: "utilities",
+    name: "Water bill",
+    dueDate: "May 15, 2019",
+    amount: 2000
+  })
+);
 
-ReactDOM.render(<AppRouter />, document.getElementById("root"));
+store.dispatch(
+  addExpense({
+    id: 2,
+    category: "utilities",
+    name: "Verizon Internet",
+    dueDate: "July 01, 2019",
+    amount: 8000
+  })
+);
+
+store.dispatch(
+  addExpense({
+    id: 3,
+    category: "health",
+    name: "Gym",
+    dueDate: "August 21, 2019",
+    amount: 2500
+  })
+);
+store.dispatch(
+  addExpense({
+    id: 4,
+    category: "credit cards",
+    name: "Citi",
+    dueDate: "June 27, 2019",
+    amount: 15000
+  })
+);
+
+const jsx = (
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>
+);
+ReactDOM.render(jsx, document.getElementById("root"));
