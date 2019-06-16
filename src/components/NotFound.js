0a026f5c0e0306463ae71e33f0570f943c4a9202
Saad Ahmed <dropheaven@live.com>
2019-06-15 21:46:24 -0400

    added container class and changed paragraph tag to h1

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -2,8 +2,8 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const NotFound = () => (
-  <div>
-    <p>404</p>
+  <div className="container text-center">
+    <h1>404</h1>
     <Link to="/">Return to Dashboard</Link>
   </div>
 );
